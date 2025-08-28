f9a78f6a855f89f97857a6079e7f0d08e2c43925
Farjana Akter Mim <farjanaaktermim330@gmail.com>
2025-08-29 01:23:58 +0600

    completed card section & responsive part

---
+++ script/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const newCart = document.createElement("div")
+//         newCart.innerHTML = `
+//           <div class="flex justify-center items-center bg-gray-200 rounded-xl">
+//                 <img
+//                   src="./assets/kitchen-1.png" alt="" class="h-[80px] pl-1"/>
+//                 <div class="card-body items-center text-center">
+//                   <h1 class="font-bold">${title}</h1>
+//                   <p class="text-gray-400 font-medium">${price} TK</p>
+//                 </div>
+//               </div>
+//             `
+
+//             cartContainer.appendChild(newCart)
